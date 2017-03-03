d211baffa3706e75f434ef4c6a80a1f66b31aa1d
emir <emir@emir-XPS-L322X>
2017-03-03 15:16:14 -0500

    Database support added Using an in-memory database to store problems

---
+++ cubes-web/src/main/java/co/com/rappi/cubes/dao/ProblemRepository.java
@@ -0,0 +1,15 @@
+package co.com.rappi.cubes.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import co.com.rappi.cubes.model.Problem;
+
+/**
+ * The repository that provides access to the problems store
+ * In these case an in-memory data base
+ * 
+ * @author <a href="emirfredy@gmail.com">Emir Cortes Trujillo</a>
+ */
+public interface ProblemRepository extends CrudRepository<Problem, Long> {
+
+}
